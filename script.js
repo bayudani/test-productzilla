155568f029c2432fd6b50a28e57291c1adc312fd
bayudani <bayu22122017@gmail.com>
2024-09-30 21:03:18 +0700

    update dark mode

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault(); 
 
     // ganti background 
-    document.querySelector(".footer").style.backgroundColor = "orange";
-    document.querySelector(".header").style.backgroundColor = "orange";
-    document.querySelector(".button").style.backgroundColor = "orange";
+    document.querySelector(".footer").style.backgroundColor = "#e74c3c";
+    document.querySelector(".header").style.backgroundColor = "#e74c3c";
+    document.querySelector(".button").style.backgroundColor = "#e74c3c";
 
     // ganti h2
     const headings = document.querySelectorAll("h2");
     headings.forEach(function(heading) {
-        heading.style.color = "orange";
+        heading.style.color = "#e74c3c";
     });
+});
+document.getElementById("darkMode").addEventListener("change", function() {
+    if (this.checked) {
+        document.body.classList.add("dark-mode");
+    } else {
+        document.body.classList.remove("dark-mode");
+    }
 });
